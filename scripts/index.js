fb818efbf6d309fdf5d4c2a79592e2bf402db8a3
Dairful <andeetang@gmail.com>
2016-10-23 23:58:38 -0700

    Restructure and add animation to navbar

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("li").hover(
+		/*
+		function() {
+			$(this).css({"background-color": "white"});
+		},
+
+		function() {
+			$(this).css({"background-color": "transparent"});
+		}
+		*/
+	);
+
+});
