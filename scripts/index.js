b4e64c95b7f75b1f1ae73ba4df574f18eb88fb13
Dairful <andeetang@gmail.com>
2016-10-25 16:23:37 -0700

    Fix navbar behaviour when clicking on the same link twice

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,36 @@ $(document).ready(function(){
 		$('html,body').animate({scrollTop: tag.offset().top}, 'slow');
 	}
 
+	/* Initialize navbar current link */
+	var previous = $(".intro");
+	previous.children("a").toggleClass("color");
+	previous.data('clicked', true);
+
 	$("li").click(function(){
-		/* Change color */
-		$(this).children("a").toggleClass("color");
 
-		/* Jump to location on page*/
-		scrollToAnchor($(this).children("a").attr("href"));
+		if (!$(this).data('clicked')) {
+  			/* Change color */
+			$(this).children("a").toggleClass("color");
+			previous.children("a").toggleClass("color");
 
-		/* Stop link from jumping to the anchor first */
-		event.preventDefault();
-  		event.stopPropagation();
-	});
+			/* Jump to location on page*/
+			scrollToAnchor($(this).children("a").attr("href"));
 
+			/* Stop link from jumping to the anchor first */
+			event.preventDefault();
+	  		event.stopPropagation();
 
+	  		/* Update previous */
+	  		previous.data('clicked', false);
+	  		$(this).data('clicked', true);
+	  		previous = $(this);
+
+  		} else {
+  			/* Stop link from jumping to the anchor first */
+			event.preventDefault();
+	  		event.stopPropagation();
+  		}
+
+	}); //end click
 
 }); //EOF
