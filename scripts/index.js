ada4e0ba942ef147e4edd820c97951f83704fdb4
Dairful <andeetang@gmail.com>
2016-10-25 00:16:44 -0700

    Animate link scroll to anchor

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
 
+	function scrollToAnchor(anchor_id){
+		var tag = $(anchor_id);
+		$('html,body').animate({scrollTop: tag.offset().top}, 'slow');
+	}
+
 	$("li").click(function(){
+		/* Change color */
 		$(this).children("a").toggleClass("color");
+
+		/* Jump to location on page*/
+		scrollToAnchor($(this).children("a").attr("href"));
+
+		/* Stop link from jumping to the anchor first */
+		event.preventDefault();
+  		event.stopPropagation();
 	});
+
+
 
 }); //EOF
